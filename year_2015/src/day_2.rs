63d6b2f660f0279b3e771cd8a492aa21e3199682
Lucas Cosson <24441517+lucas-cosson@users.noreply.github.com>
2022-11-04 14:18:39 +0100

    feat: add day2

--- year_2015/src/day_2.rs
+++ year_2015/src/day_2.rs
@@ -1,5 +1,39 @@
 pub fn day2_part1() {
+	let input = lib::handle_input_file("input/year_2015/day_2.txt");
+	let split_string: Vec<&str> = input.split("\n").collect();
+	let mut total = 0;
+
+	for str in split_string {
+		let dimensions: Vec<&str> = str.split("x").collect();
+		let l = dimensions[0].parse::<i32>().unwrap();
+		let w = dimensions[1].parse::<i32>().unwrap();
+		let h = dimensions[2].parse::<i32>().unwrap();
+
+		let mut sides = vec![l*w, w*h, h*l];
+		sides.sort();
+
+		total += sides.iter().fold(0, |acc, x| acc + 2*x);
+		total += sides[0];
+	}
+
+	println!("Day 2 - Part 1: Square feet of wrapping paper: {}", total);
 }
 
 pub fn day2_part2() {
+	let input = lib::handle_input_file("input/year_2015/day_2.txt");
+	let split_string: Vec<&str> = input.split("\n").collect();
+	let mut total = 0;
+
+	for str in split_string {
+		let mut dimensions = str.split("x")
+			.collect::<Vec<&str>>()
+			.iter()
+			.map(|x| x.parse::<i32>().unwrap())
+			.collect::<Vec<i32>>();
+		dimensions.sort();
+
+		total += 2*dimensions[0] + 2*dimensions[1] + dimensions.iter().fold(1, |acc, x| acc * x);
+	}
+
+	println!("Day 2 - Part 2: Feet of ribbon: {}", total);
 }
--- year_2015/src/main.rs
+++ year_2015/src/main.rs
@@ -6,7 +6,13 @@ fn main() {
 	day_1::day1_part1();
 	day_1::day1_part2();
 
+	println!();
+
 	// Day 2
 	day_2::day2_part1();
 	day_2::day2_part2();
+
+	println!();
+
+	// Day 3
 }
